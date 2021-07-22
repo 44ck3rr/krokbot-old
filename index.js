9c74b2a99da2069220c94d3740c687e208a02e68
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-07-22 06:43:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client;
+
+const prefix = '=';
+
+client.once('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
+})
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix + 'play')){
+        if(message.member.voice.channel){
+            if(message.member.voice.channel.id = ('657697993423716355')){
+                message.member.voice.channel.join().then(connection => {
+                    let args = message.content.split(' ');
+
+                    if(!args[1]){
+                        message.reply('Url invalide')
+                  }
+                  else {
+                    
+                  let dispatcher = connection.play(ytdl(args[1], { quality: "highestaudio" }));
+
+                 dispatcher.on('finish', () => {
+                     dispatcher.destroy();
+                 })
+
+                 dispatcher.on('error', err => {
+                      console.log('Erreur de dispatcher : ' + err);
+                  })
+                  }
+               }).catch(err => {
+                 message.reply('Erreur lors de la connexion : ' + err);
+                 })
+            }
+            else {
+                message.reply("Vous n'êtes pas dans le salon vocal 🎶・Music")
+            }   
+        }
+        
+        else {
+            message.reply("Vous n'êtes pas connecté en vocal");
+        }
+    }
+})
+
+client.on('error', (error) => console.error());
+
+client.login('ODY3NDkwMjkyOTAyMTMzNzcw.YPh3Sw.rJqB1NpFE6de2E0cQdVttTtMo9s')
