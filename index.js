be00931e42ac073b49fbea985cb24eb7febc441b
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-07-31 18:10:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.once('ready', () => {
     setInterval(() => {
         client.user.setActivity(status[i], {type: 'STREAMING', url: 'https://twitch.tv/NyanCat'})
         i = ++i % status.length
-    }, 1e4)
+    }, 400)
 })
 
 client.once('ready', () => {
