6a1819db3025c83aade2c10767ac8b629bd95262
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-07-31 17:33:29 +0200

    Add files via upload

---
+++ commands/event.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bdd = require('../bdd.json');
+const fs = require('fs');
+module.exports.run = async (bot, message, args) => {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return;
+        bot.channel.cache.get(bdd['channel-events']).send('Concours (Test)').then(message => {
+            message.react('✨');
+            bdd['message-event-1k'] = message.id;
+            Savebdd;
+        });
+}
+function Savebdd() {
+    fs.writeFile('../bdd.json', JSON.stringify(bdd, null, 4), (err) => {
+        if (err) message.channel.send('Une erreur est survenue.');
+    });
+}
+module.exports.config = {
+    name: 'event'
+}
