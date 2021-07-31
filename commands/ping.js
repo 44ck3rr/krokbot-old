76d028f2ef81130bc727316e55208705dc9f3778
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-01 00:15:06 +0200

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+
+    message.delete();
+    if(message.author.bot) return
+
+    let msg = await message.channel.send('Calcule en cours...')
+    msg.delete();
+
+    let PING = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle('PONG !')
+    .setDescription('Voici les résultats.')
+    .addField('Ma latence est de :', Math.floor(msg.createAt - message.createAt)+ " " + "ms")
+    message.channel.send(PING)
+}
+
+module.exports.config = {
+    name: 'ping'
+}
