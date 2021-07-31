81b40f6db98f6bfeb037c6e9ba180f225098f228
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-07-31 17:59:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const prefix = '.';
 
 client.once('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
+    
+    const status = [
+        'B',
+        'Bo',
+        'Bon',
+        'Bonj',
+        'Bonjo',
+        'Bonjou',
+        'Bonjour',
+        'Bonjour !'
+    ]
+    let i = 0
+    setInterval(() => {
+        client.user.setActivity(status[i], {type: 'STREAMING', url: 'https://twitch.tv/NyanCat'})
+        i = ++i % status.length
+    }, 1e4)
+})
+
+client.once('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
 })
 
 client.on('error', (error) => console.error());
