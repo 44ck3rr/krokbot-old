d957af860a646ba89efc6824ec597d37be9a8fe4
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-07-31 18:54:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 
-const client = new Discord.Client;
+const client = new Discord.Client({
+    fetchAllMembers: true
+}),
+config = require('./config.json')
+fs = require('fs')
 
 const prefix = '.';
 
@@ -16,13 +20,28 @@ client.once('ready', () => {
     setInterval(() => {
         client.user.setActivity(status[i], {type: 'STREAMING', url: 'https://twitch.tv/NyanCat'})
         i = ++i % status.length
-    }, 5000)
+    }, 400)
 })
-
-client.once('ready', () => {
-    console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
-})
-
-client.on('error', (error) => console.error());
 
 client.login(process.env.TOKEN);
+client.commands = new Discord.Collection()
+
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${files}`)
+        client.commands.set(command.name, command)
+    })
+})
+
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
