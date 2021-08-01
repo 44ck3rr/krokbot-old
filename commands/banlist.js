fb4977f1bbd993e004bd12c390ed660a797fb2d1
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-01 16:55:30 +0200

    Add files via upload

--- commands/banlist.js
+++ commands/banlist.js
@@ -3,9 +3,11 @@ const Discord = require('discord.js')
 module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission('BAN_MEMBERS')) return
     const fetchBans = message.guild.fetchBans();
-    const bannedMembers = (await fetchBans).map((member) => member.user.tag).join(', ')
+    const bannedMembers = (await fetchBans).map((member) => member.user.tag).join('\n')
 
-    message.channel.send(bannedMembers)
+    message.channel.send(new Discord.MessageEmbed()
+    .setTitle('Liste des membres bannis')
+    .setDescription(bannedMembers))
 
 }
 
