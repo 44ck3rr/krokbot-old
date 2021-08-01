f9f8cf9c0e49c46ae5c8a2d3fd9f31e35525bf96
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-01 15:58:25 +0200

    Add files via upload

---
+++ commands/sayEmbed.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande !')
+    const SayEmbed = args.join(" ");
+    if(message.author.bot) return
+    message.delete();
+    message.channel.send(new Discord.MessageEmbed()
+    .setTitle(SayEmbed))
+}
+
+module.exports.config = {
+    name: 'sayEmbed'
+}
