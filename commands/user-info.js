e3521dce58daaab53a87a04b87a7fdbbbd3d8b9b
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-01 02:32:33 +0200

    Add files via upload

--- commands/user-info.js
+++ commands/user-info.js
@@ -6,9 +6,6 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(new Discord.MessageEmbed()
         .addField('Membre', member, true)
         .addField('Tag', member.user.tag, true)
-        .addField('Date de création du compte', moment(member.user.createdAt).format(`[Le] DD/MM/YYYY [à] HH:mm:ss`), true)
-        .addField('Date d\'arrivée sur le serveur', moment(member.joinedAt).format(`[Le] DD/MM/YYYY [à] HH:mm:ss`), true)
-        .addField('Date de début de boost', member.premiumSince ? moment(member.premiumSince).format(`[Le] DD/MM/YYYY [à] HH:mm:ss`) : 'Ne boost pas', true)
         .setThumbnail(member.user.displayAvatarURL())
         .setFooter(`ID : ${member.id}`))
 },
