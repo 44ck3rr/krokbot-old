faebc1970532914b5f1dfcfec10356b50a3532b4
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-02 00:12:37 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js')
+const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
     message.delete()
     message.channel.send(new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
-    .setDescription('**Prefixe:** .\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918')
+    .setDescription(`**Prefixe:** ${config.prefix}\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918`)
     .addField('__Admin Commands__', 'say\nsayEmbed\nclear\nnuke\nbanList', true)
     .addField('__Other__', 'help\nping\nuser-info\nserver-info', true)
     .setThumbnail(message.guild.iconURL())
