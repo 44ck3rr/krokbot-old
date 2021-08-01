55254ebe5857e35de4fd0b51b81cf2d5fb108a89
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-01 15:42:41 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
-    .setDescription('**Prefixe: .**\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918')
+    .setDescription('**Prefixe:** .\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918')
     .addField('__Admin Commands__', '.say\n.clear', true)
     .addField('__Other__', 'help\nping\nuser-info\nserver-info', true)
     .setThumbnail(message.guild.iconURL()))
