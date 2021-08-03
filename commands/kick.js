8481aa908d4f942addada5827e81432a723ef2e4
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-03 10:43:57 +0200

    Add files via upload

--- commands/kick.js
+++ commands/kick.js
@@ -4,6 +4,8 @@ module.exports.run = async (client, message, args) => {
     const user = message.mentions.users.first();
     const reason = args.splice(1).join(' ');
     user ? message.guild.member(user).kick(reason) : message.channel.send('Je n\'ai pas trouvé cette utilisateur.')
+    if (!user) return
+    message.channel.send('Ban réalisé')
 }
 
 module.exports.config = {
