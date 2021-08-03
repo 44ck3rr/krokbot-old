fa313f68c5ff069b028e1beac8ffc9c8539f7923
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-03 10:48:28 +0200

    Add files via upload

--- commands/kick.js
+++ commands/kick.js
@@ -5,7 +5,15 @@ module.exports.run = async (client, message, args) => {
     const reason = args.splice(1).join(' ');
     user ? message.guild.member(user).kick(reason) : message.channel.send('Je n\'ai pas trouvé cette utilisateur.')
     if (!user) return
-    message.channel.send('Ban réalisé')
+    if (!reason) reason = 'Non-spécifié';
+    message.channel.send(new Discord.MessageEmbed()
+    .setTitle('Membre Exclu')
+    .setThumbnail(member.user.displayAvatarURL())
+    .addField('Utilisateur exclu', member)
+    .addField('Exclu par', message.author)
+    .addField('Raison', reason)
+    .setFooter('Pour toute erreur d\'exclusion, contactez 4_4ck3r#4918', client.user.displayAvatarURL())
+    .setTimestamp())
 }
 
 module.exports.config = {
