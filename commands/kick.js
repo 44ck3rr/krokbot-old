de10f1036122ec4a3663f5e740057fb506e7fb6b
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-03 11:11:02 +0200

    Add files via upload

--- commands/kick.js
+++ commands/kick.js
@@ -2,30 +2,40 @@ const Discord = require('discord.js')
 const client = new Discord.Client
 
 module.exports.run = async (client, message, args) => {
-    const user = message.mentions.users.first();
-    const reason = args.splice(1).join(' ');
-    user ? message.guild.member(user).kick(reason) : message.channel.send('Je n\'ai pas trouvé cette utilisateur.')
-    if (!user) return
-    if (!reason) reason = 'Non-spécifié';
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send('Je n\'ai pas la permission de faire celà')
 
-    message.channel.send(new Discord.MessageEmbed()
-    .setTitle('Membre Exclu')
-    .setThumbnail(member.user.displayAvatarURL())
-    .addField('Utilisateur exclu', member)
-    .addField('Exclu par', message.author)
-    .addField('Raison', reason)
-    .setFooter('Pour toute erreur d\'exclusion, contactez 4_4ck3r#4918', client.user.displayAvatarURL())
-    .setTimestamp())
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-    let modMessage = client.channels.cache.get('857648787090178099');
-    modMessage.send(new Discord.MessageEmbed()
-    .setTitle('Membre Exclu')
-    .setThumbnail(member.user.displayAvatarURL())
-    .addField('Utilisateur exclu', member)
-    .addField('Exclu par', message.author)
-    .addField('Raison', reason)
-    .setFooter('Pour toute erreur d\'exclusion, contactez 4_4ck3r#4918', client.user.displayAvatarURL())
-    .setTimestamp())
+        if(!args[0]) return message.channel.send('Merci de mentionné l\'utilisateur a bannir');
+
+        if(!member) return message.channel.send('Je n\'ai pas trouvé cette utilisateur.');
+        if(!member.kickable) return message.channel.send('Ce membre ne peut pas être exclu.');
+
+        if(member.id === message.author.id) return message.channel.send('Pourquoi vous exclure vous meme 🤔');
+
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = 'Non-spécifié';
+
+        member.kick({reason: reason}).catch(err => { 
+          message.channel.send('Erreur')
+            console.log(err)
+        })
+
+        const kickembed = new Discord.MessageEmbed()
+        .setTitle('Membre Exclu')
+        .setThumbnail(member.user.displayAvatarURL())
+        .addField('Utilisateur exclu', member)
+        .addField('Exclu par', message.author)
+        .addField('Raison', reason)
+        .setFooter('Pour toute erreur d\'exclusion, contactez 4_4ck3r#4918', client.user.displayAvatarURL())
+        .setTimestamp()
+
+        message.channel.send(kickembed);
+
+        let modMessage = client.channels.cache.get('857648787090178099');
+        modMessage.send(kickembed);
 
 }
 
