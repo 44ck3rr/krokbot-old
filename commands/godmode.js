0c444cc44941343f99f564d3832e56cdcf0dbe66
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-04 04:21:37 +0200

    Add files via upload

---
+++ commands/godmode.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    let GodMode = 'GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}'
+    message.channel.send(new Discord.MessageEmbed()
+    .setTitle('Commande Secrète')
+    .setDescription(`Astuce: Renomme un nouveau dossier avec ceci :\n${GodMode}`))
+}
+
+module.exports.config = {
+    name: 'GodMode'
+}
