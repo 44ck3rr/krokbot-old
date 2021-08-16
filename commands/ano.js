2346c468365c8b726123844271b26e8ed6c2e1db
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-16 13:12:02 +0200

    Add files via upload

--- commands/ano.js
+++ commands/ano.js
@@ -3,14 +3,26 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
     const anoMsg = args.join(" ");
     if(message.author.bot) return
+    const anoAuthor = message.author()
     message.delete();
+    
+    const anoLogsMsg = new Discord.MessageEmbed()
+    .setAuthor(anoAuthor)
+    .setTitle(`Message anonyme de ${anoAuthor}`)
+    .setDescription(anoMsg)
+    .setTimestamp() 
+
     const anoEmbed = new Discord.MessageEmbed()
     .setTitle('Message anonyme !')
     .setDescription(anoMsg)
     .setTimestamp()
 
     let anoChannel = client.channels.cache.get('876781983617937468')
+    let anoLogs = client.channels.cache.get('876783871620939816')
+
     anoChannel.send(anoEmbed)
+    anoLogs.send(anoLogsMsg)
+
 }
 
 module.exports.config = {
