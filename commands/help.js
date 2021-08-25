c7c4ed3fa6aafbe56e14f3def1ae46b278e28ed4
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-25 12:30:10 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -2,16 +2,18 @@ const Discord = require('discord.js')
 const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
-    message.delete()
-    message.channel.send(new Discord.MessageEmbed()
+    helpEmbed = new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
-    .setDescription(`**Prefixe:** ${config.prefix}\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918`)
+    .setDescription(`**Prefixe:** Universelle\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918`)
     .addField('__Admin Commands__', 'say\nsayEmbed\nclear\nnuke\nbanList\nban\nkick', true)
-    .addField('__Other__', 'help\nping\nuser-info\nserver-info', true)
+    .addField('__Other__', 'help\nping\nuser-info\nserver-info\nano', true)
     .addField('__Secret__', '1 commande secrète est disponible.', true)
     .setThumbnail(message.guild.iconURL())
-    .setFooter(`${message.author.tag()} - © Tous droits réservés`, message.author.displayAvatarURL()))
+    .setFooter(`${message.author.tag()} - © Tous droits réservés`, message.author.displayAvatarURL())
+
+    message.delete()
+    message.channel.send(helpEmbed)
 }
 
 module.exports.config = {
