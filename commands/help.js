33702daa85691f1e1f3425003c6c1850f0a7498d
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-25 12:33:41 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
-    helpEmbed = new Discord.MessageEmbed()
+    const helpEmbed = new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
     .setDescription(`**Prefixe:** Universelle\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918`)
@@ -12,6 +12,7 @@ module.exports.run = async (client, message, args) => {
     .setThumbnail(message.guild.iconURL())
     .setFooter(`${message.author.tag()} - © Tous droits réservés`, message.author.displayAvatarURL())
 
+    message.delete()
     message.channel.send(helpEmbed)
 }
 
