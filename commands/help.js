5508fbfdbe110fd2c78064f32f71e2cbe5966bfa
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-25 12:38:43 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const config = require('../config.json')
 module.exports.run = async (client, message, args) => {
     message.delete()
     
-    message.channel.send(Discord.MessageEmbed()
+    message.channel.send(new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
     .setDescription(`**Prefixe:** Universelle\n**Code Language:** NodeJs\n**Dev by:** 4_4ck3r#4918`)
