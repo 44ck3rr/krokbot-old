8f280a755c39fab1fc04fbddd240e312c5f34b0b
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-31 18:55:02 +0200

    Add files via upload

--- commands/server-info.js
+++ commands/server-info.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(new Discord.MessageEmbed()
     .addField('Nom', message.guild.name, true)
     .addField('Région', message.guild.region, true)
-    .addField('Membres', `${message.guild.memberCount} Autistes`, true)
+    .addField('Membres', `${message.guild.memberCount} Membres`, true)
     .addField('Salons', `${message.guild.channels.cache.size} salons\n${message.guild.channels.cache.filter(channel => channel.type === 'text').size} salons textuels\n${message.guild.channels.cache.filter(channel => channel.type === 'voice').size} salons vocaux\n${message.guild.channels.cache.filter(channel => channel.type === 'category').size} catégories`, true)
     .addField('Emojis', `${message.guild.emojis.cache.size} emojis\n${message.guild.emojis.cache.filter(emoji => !emoji.animated).size} emojis statiques\n${message.guild.emojis.cache.filter(emoji => emoji.animated).size} emojis animés`, true)
     .addField('Rôles', message.guild.roles.cache.size, true)
