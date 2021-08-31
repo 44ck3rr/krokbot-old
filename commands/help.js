73fcdefacc4c9aa32e7311c893b5826b112dc2fb
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-08-31 20:34:50 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     message.channel.send(new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
-    .setDescription(`**🏷・Prefixe:** Universelle\n**🗣・Code Language:** NodeJs\n**⚙・Dev by:** 4_4ck3r#4918\n**🌐・Site:** https://votretaverne.hubside.fr/bot \n**❓・Support:** https://discord.gg/7tgVRJpXCu `)
+    .setDescription(`**🏷・Prefixe:** Universelle\n**<:nodejs:882332390574420088>・Code Language:** NodeJs\n**⚙・Dev by:** 4_4ck3r#4918\n**🌐・Site:** https://votretaverne.hubside.fr/bot \n**❓・Support:** https://discord.gg/7tgVRJpXCu `)
     .addField('🔰・__Admin Commands__', 'say\nsayEmbed\nclear\nnuke\nbanList\nban\nkick', true)
     .addField('🔆・__Other__', 'help\nping\nuser-info\nserver-info\nano', true)
     .addField('🔒・__Secret__', '1 commande secrète est disponible.', true)
