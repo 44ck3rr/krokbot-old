c3a56e1f82ee762e454baabf5da46ccfbeb04712
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-09-01 15:57:26 +0200

    Add files via upload

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,13 +5,11 @@ module.exports.run = async (client, message, args) => {
     const question = args.join(" ");
     const reponse = Math.floor(Math.random() * replies.length);
 
-    const embed = new Discord.MessageEmbed()
+    message.channel.send(new Discord.MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL())
     .setColor("#cb4e41")
     .setThumbnail("https://cdn.discordapp.com/attachments/882622734507593818/882623617932881950/unnamed.png")
-    .addField(question, replies[reponse]);
-
-    message.channel.send(embed)
+    .addField(question, replies[reponse]))
 }
 
 module.exports.config = {
