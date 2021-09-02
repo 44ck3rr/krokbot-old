24e909a950efdc996162803e15b3aed775457787
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-09-02 17:51:30 +0200

    Add files via upload

--- commands/test.js
+++ commands/test.js
@@ -1,14 +1,13 @@
-/*const db = require('quick.db')
+//const db = require('quick.db')
 const Discord = require('discord.js')
 const economy = new db.table('Economy')
-
+/*
 module.exports.run = async (client, message, args) => {
     let token = economy.fetch(`money_${message.guild.id}_${user.id}`)
     if (token === null) token = 0
     return message.channel.send(`${token} **Tokens**`)
 }
-
+*/
 module.exports.config = {
     name: 'test'
 }
-*/
