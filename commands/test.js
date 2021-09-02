8e29f4535b69c9f7578ba5b51b106ea918c3c2e6
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-09-02 17:38:08 +0200

    Add files via upload

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,11 @@
+const db = require('quick.db')
 const Discord = require('discord.js')
+const economy = new db.table('Economy')
 
 module.exports.run = async (client, message, args) => {
-    const Embed = new Discord.MessageEmbed()
-    .setColor('#10eee2')
-    .setTitle('Ceci est un Test')
-    .setDescription('Description test')
-    .addField('Field test', 'test')
-    .setTimestamp();
-
-    message.channel.send(Embed);
-
+    let token = economy.fetch(`money_${message.guild.id}_${user.id}`)
+    if (token === null) token = 0
+    return message.channel.send(`${token} **Tokens**`)
 }
 
 module.exports.config = {
