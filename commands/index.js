74a9e96927adb974a3274760ae66916d24fb1b58
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-09-02 17:10:20 +0200

    Add files via upload

---
+++ commands/index.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+const client = new Discord.Client();
+const config = require('./config.json');
+const prefix = config.prefix
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+mongoose.connect('mongodb+srv://TaverneAutistes:tigrouprincecharly@tokens.2dt4j.mongodb.net/token_economy?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
+
+fs.readdir('./commands/', (err, files ) => {
+    if(err) console.log(err);
+    let jsfile = files.filter(f => f.split(".").pop() === 'js');
+    if(jsfile.length <= 0) {
+        console.log('Aucune commande n\'a été trouvée dans le Handler')
+    }
+
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`HANDLER: ${f} a été detecter.`)
+        client.commands.set(props.config.name, props)
+    })
+})
+
+client.on('message', async message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.splice(1);
+
+    let commandFile = client.commands.get(command.slice(prefix.length))
+    if(commandFile) commandFile.run(client, message, args)
+})
+
+client.on('ready', async () => {
+    console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
+    let statuses = [
+        'Dev by 4_4ck3r',
+        '.help'
+    ]
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        client.user.setActivity(status, {type: 'STREAMING'})
+    }, 5000)
+})
+
+client.login(process.env.TOKEN);
