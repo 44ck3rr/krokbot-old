6f7ca5375e8b3a1c3dda464ab83debc1391a00c2
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-10-30 07:15:04 +0200

    Add files via upload

--- commands/ano.js
+++ commands/ano.js
@@ -2,22 +2,24 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 
-    if (!message.channel.parentID == '676065537125515284') return message.channel.send('Cette commande est utilisable seulement sur La Taverne des Autistes dans la section "Tavernes".')
-    const anoMsg = args.join(" ");
-    if(message.author.bot) return
-    if(!args[0]) return message.channel.send('Aucun message indiquer')
-    message.delete();
+    if(!message.channelId == '872591585525780530')
+        message.channel.send('Cette commande est disponible seulement sur La Taverne des Autistes dans le salon "<#872591585525780530>"')
+    else {
+        const anoMsg = args.join(" ");
+        if(message.author.bot) return
+        if(!args[0]) return message.channel.send('Aucun message indiquer')
+        message.delete();
 
-    const anoEmbed = new Discord.MessageEmbed()
-    .setTitle('Message anonyme !')
-    .setDescription(anoMsg)
-    .setTimestamp()
+        const anoEmbed = new Discord.MessageEmbed()
+        .setTitle('Message anonyme !')
+        .setDescription(anoMsg)
+        .setTimestamp()
 
-    let anoChannel = client.channels.cache.get('876781983617937468')
-    let anoLogs = client.channels.cache.get('876783871620939816')
+        let anoChannel = client.channels.cache.get('876781983617937468')
+        let anoLogs = client.channels.cache.get('876783871620939816')
 
-    anoChannel.send(anoEmbed)
-
+        anoChannel.send(anoEmbed)
+    }
 }
 
 module.exports.config = {
