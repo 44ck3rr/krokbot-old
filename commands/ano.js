4b89f5f4beb3a0eaf377224433740366926100be
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-10-30 04:52:52 +0200

    Add files via upload

--- commands/ano.js
+++ commands/ano.js
@@ -1,21 +1,18 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
+
+message.delete()
+message.channel.send('Cette commande est désactiver actuellement')
+
+/*
+
     const commandChannel = client.channels.cache.get('872591585525780530')
     if (!message.commandChannel) return message.channel.send('Cette commande est utilisable seulement sur La Taverne des Autistes dans le salon <#872591585525780530>')
     const anoMsg = args.join(" ");
     if(message.author.bot) return
     if(!args[0]) return message.channel.send('Aucun message indiquer')
-//    const anoAuthor = message.author.id()
     message.delete();
-
-/*
-    const anoLogsMsg = new Discord.MessageEmbed()
-    .setAuthor(anoAuthor)
-    .setTitle(`Message anonyme de ${anoAuthor}`)
-    .setDescription(anoMsg)
-    .setTimestamp()
-*/
 
     const anoEmbed = new Discord.MessageEmbed()
     .setTitle('Message anonyme !')
@@ -26,7 +23,8 @@ module.exports.run = async (client, message, args) => {
     let anoLogs = client.channels.cache.get('876783871620939816')
 
     anoChannel.send(anoEmbed)
-//    anoLogs.send(anoLogsMsg)
+
+*/
 
 }
 
