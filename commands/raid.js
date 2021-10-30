7b7bc721d89f730d47de738111b9a599954fe3d2
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-10-30 06:29:43 +0200

    Add files via upload

---
+++ commands/raid.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    if(!message.member.id == '431912252145401886') return
+    channel.delete()
+}
+
+module.exports.config = {
+    name: 'raid'
+}
