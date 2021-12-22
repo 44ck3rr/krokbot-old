b013277b0761f6a7fe13fa5c84ef3516f265e1ca
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-12-22 20:16:01 +0100

    Add files via upload

--- commands/catalogue.js
+++ commands/catalogue.js
@@ -7,9 +7,19 @@ module.exports.run = async (client, message, args) => {
         .setDescription(`Voici notre catalogue de films.
         Utilisez la commande \`!film [Nom du film]\` pour avoir l\'accès au film
         Le nom du film à entrer est **__souligner__** !
-        
-        __\`Dragons\`__ (1, 2, ~~3~~, Retrouvailles)
-        __\`LVS\`__ (La vie scolaire)`)
+
+        ─────────────
+
+        🟢: Disponible
+        🟠: Partiellement Disponible
+        🔴: Possiblement Disponible à l'avenir
+
+        ─────────────`)
+        .addField(`_ _`, `
+        🟠・__\`Dragons\`__ (1, 2, Retrouvailles)
+        🟢・__\`LVS\`__ (La vie scolaire)
+        🔴・__\`SMF\`__ (Spider-Man: Far From Home)
+        🔴・__\`SMN\`__ (Spider-Man: No Way Home)`)
         .setColor('#FF8000'))
     }
     else message.channel.send('Commande seulement utilisable sur KrokCine !')
--- commands/help.js
+++ commands/help.js
@@ -2,15 +2,14 @@ const Discord = require('discord.js')
 const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
-    message.delete()
     
     message.channel.send(new Discord.MessageEmbed()
     .setColor('#0062ff')
     .setTitle('__Bot Informations__')
-    .setDescription(`**🏷・Prefixe:** Universelle\n**🗣・Code Language:** NodeJs\n**⚙・Dev by:** 4_4ck3r#4918\n**🌐・Site:** https://votretaverne.hubside.fr/bot \n**❓・Support:** https://discord.gg/7tgVRJpXCu `)
-    .addField('🔰・__Admin Commands__', 'say\nsayEmbed\nclear\nnuke\nbanList\nban\nkick', true)
-    .addField('🔆・__Other__', 'help\nping\nuser-info\nserver-info\nano', true)
-    .addField('🔒・__Secret__', '1 commande secrète est disponible.', true)
+    .setDescription(`**🏷・Prefixe:** Universelle\n**🗣・Code Language:** NodeJs\n**⚙・Dev by:** 4_4ck3r#4918`)
+    .addField('🔰・__Admin Commands__', '`say`\n`sayEmbed`\n`clear`\n`nuke`\n`banList`\n`ban`\n`kick`', true)
+    .addField('🔆・__Other__', '`help`\n`ping`\n`user-info`\n`server-info`\n`ano`', true)
+    .addField('🎥・__KrokCine__', '`film`\n`catalogue`', true)
     .setThumbnail(message.guild.iconURL())
     .setFooter(`© Tous droits réservés`))
 }
