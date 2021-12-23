56dbc671333cacf2f6c6bbdce35d3ab622f520e3
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-12-23 17:52:10 +0100

    Add files via upload

--- commands/film.js
+++ commands/film.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send('Cette commande est désactiver pour le moment.')
-/*    if (message.guild.id == 908063418727165982){
+/*    message.channel.send('Cette commande est désactiver pour le moment.')
+    if (message.guild.id == 908063418727165982){
         const film = args.join(" ")
         if (!film) return message.channel.send('Aucun film indiquer')
         if(args[0] === 'LVS'){
@@ -26,6 +26,16 @@ module.exports.run = async (client, message, args) => {
                 message.channel.send('Réussie ! Accès retiré.')
             }
             
+        }
+        else if(args[0] === 'SMF'){
+            if(!message.member.roles.cache.has('923616649825898537')){
+                message.member.roles.add('923616649825898537')
+                message.channel.send('Réussie ! Accès ajouté.')
+            }
+            else{
+                message.member.roles.remove('923616649825898537')
+                message.channel.send('Réussie ! Accès retiré.')
+            }
         }
         else message.channel.send(`Ce film n'est pas dans notre catalogue.`)
     }
