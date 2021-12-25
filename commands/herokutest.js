0d295242a19b1f8528bd40a308362ccb004bfbca
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2021-12-25 23:12:12 +0100

    Add files via upload

--- commands/herokutest.js
+++ commands/herokutest.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send(process.env.TEST)
+    process.env.TEST
 }
     
 module.exports.config = {
