43ae9c5e3940d293531fb9f9c9d3721ba88fa86d
44ck3rr <87782696+44ck3rr@users.noreply.github.com>
2022-01-02 15:57:29 +0100

    Add files via upload

--- commands/catalogue.js
+++ commands/catalogue.js
@@ -18,12 +18,18 @@ module.exports.run = async (client, message, args) => {
         ─────────────`)
         .addField(`_ _`, `
         🟠・__\`Dragons\`__ ([1](https://www.youtube.com/watch?v=rsj6Iv2jE1I), [2](https://www.youtube.com/watch?v=zizhykXsrPI), [Retrouvailles](https://www.youtube.com/watch?v=twYrNKt2Vbw))
+        🟠・__\`Tuches\`__ ([1](https://www.youtube.com/watch?v=ndCzT6dyOqk), [2](https://www.youtube.com/watch?v=pKbox8S-se4), [3](https://www.youtube.com/watch?v=WqSPFOLA9sE), [4](https://www.youtube.com/watch?v=vxWSTApFmR4))
         🟢・__\`LVS\`__ ([La vie scolaire](https://www.youtube.com/watch?v=n05KRM4Qbq0))
         🟢・__\`SMF\`__ ([Spider-Man: Far From Home](https://www.youtube.com/watch?v=FguIk-SEkWI))
-        🔴・__\`SMN\`__ ([Spider-Man: No Way Home](https://www.youtube.com/watch?v=7w_w10HVa54))
-        🟢・__\`Afond\`__ ([A fond](https://www.youtube.com/watch?v=Hh29AkftLn8))
+        🟢・__\`Inseparables\`__ ([Inséparables](https://www.youtube.com/watch?v=Dd-r11c42xQ))
+        🟢・__\`Afond\`__ ([A fond](https://www.youtube.com/watch?v=Hh29AkftLn8))`)
+        .addField(`_ _`, `
         🟢・__\`FastHS\`__ ([Fast & Furious: Hobbs & Shaw](https://www.youtube.com/watch?v=NfCiX81lcDU))
-        🔴・__\`Venom\`__ ([1](https://www.youtube.com/watch?v=Pw9ZvTicmIg), [2](https://www.youtube.com/watch?v=eUkslWrf1Ds))`)
+        🟠・__\`Venom\`__ ([1](https://www.youtube.com/watch?v=Pw9ZvTicmIg), [2](https://www.youtube.com/watch?v=eUkslWrf1Ds))
+        🔴・__\`BlackWidow\`__ ([Black Widow](https://www.youtube.com/watch?v=4l99M0zOEaA))
+        🔴・__\`Avengers\`__ ([1](https://www.youtube.com/watch?v=b-kTeJhHOhc), [2](https://www.youtube.com/watch?v=Z0mPyiZ23x8), [3](https://www.youtube.com/watch?v=eIWs2IUr3Vs), [4](https://www.youtube.com/watch?v=wV-Q0o2OQjQ))
+        🔴・__\`SpiderMan\`__ ([1](https://www.youtube.com/watch?v=rcVNkgpLhPM), [2](https://www.youtube.com/watch?v=4ZIekwOMhdU), [3](https://www.youtube.com/watch?v=qK7nMGNlHgk&t))
+        🔴・__\`SpiderAmazing\`__ ([1](https://www.youtube.com/watch?v=mJlDpguadbk), [2](https://www.youtube.com/watch?v=Vd3YX3O6P_8))`)
         .setColor('#FF8000'))
     }
     else message.channel.send(new Discord.MessageEmbed()
--- commands/film.js
+++ commands/film.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send('Cette commande est désactiver pour le moment.')
-/*    if (message.guild.id == 908063418727165982){
+    if (message.guild.id == 908063418727165982){
         const film = args.join(" ")
         if (!film) return message.channel.send('Aucun film indiquer')
         if(args[0] === 'LVS'){
@@ -57,10 +56,40 @@ module.exports.run = async (client, message, args) => {
                 message.channel.send('Réussie ! Accès retiré.')
             }
         }
+        else if(args[0] === 'Tuches'){
+            if(!message.member.roles.cache.has('927186585550725230')){
+                message.member.roles.add('927186585550725230')
+                message.channel.send('Réussie ! Accès ajouté.')
+            }
+            else{
+                message.member.roles.remove('927186585550725230')
+                message.channel.send('Réussie ! Accès retiré.')
+            }
+        }
+        else if(args[0] === 'Inseparables'){
+            if(!message.member.roles.cache.has('927190236193259561')){
+                message.member.roles.add('927190236193259561')
+                message.channel.send('Réussie ! Accès ajouté.')
+            }
+            else{
+                message.member.roles.remove('927190236193259561')
+                message.channel.send('Réussie ! Accès retiré.')
+            }
+        }
+        else if(args[0] === 'Venom'){
+            if(!message.member.roles.cache.has('927202473045786695')){
+                message.member.roles.add('927202473045786695')
+                message.channel.send('Réussie ! Accès ajouté.')
+            }
+            else{
+                message.member.roles.remove('927202473045786695')
+                message.channel.send('Réussie ! Accès retiré.')
+            }
+        }
         else message.channel.send(`Ce film n'est pas dans notre catalogue.`)
     }
     else message.channel.send(new Discord.MessageEmbed()
-    .setDescription('Commande seulement utilisable sur [KrokCine](https://discord.gg/E4KSjhv8WR) !'))*/
+    .setDescription('Commande seulement utilisable sur [KrokCine](https://discord.gg/E4KSjhv8WR) !'))
 }
 
 module.exports.config = {
